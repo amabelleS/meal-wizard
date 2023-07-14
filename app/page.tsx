0d9ac593c444f294b48888b190c7dfe6b7e50e3c
amabelleS <amabelletr@gmail.com>
2023-07-14 18:30:49 +0300

    styles & loading

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,14 @@ import { getRecipes } from '@/lib/getRecipes';
 export default function Home() {
   const [products, setProducts] = useState('');
   const [recipes, setRecipes] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
   
   const handleProductChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setProducts(event.target.value);
   };
   
   const handleGenerateRecipes = async () => {
+    setLoading(true);
     try {
       const recipes = await getRecipes({products});
       console.log("🚀 ~ file: page.tsx:23 ~ handleGenerateRecipes ~ recipes:", recipes)
@@ -35,19 +37,21 @@ export default function Home() {
     } catch (error) {
       console.error('Error:', error);
     }
+    setLoading(false);
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="flex justify-between items-center mt-8 mb-6">
-        <h1 className="text-3xl font-bold">Meal Wizard</h1>
+    <div className="container mx-auto p-8">
+      <div className="flex justify-between items-center mt-4 mb-6">
+        <h1 className="text-3xl font-bold text-teal-400">Meal Wizard</h1>
         <Image
             src="https://upload.wikimedia.org/wikipedia/commons/4/4d/OpenAI_Logo.svg"
             width={88}
             height={77}
             alt="Openai logo"
-            className='hover:motion-safe:animate-pulse text-white'
-            style={{ filter: 'invert(1)' }}
+            className='hover:motion-safe:animate-pulse text-teal-400'
+            // style={{ filter: 'invert(1)', color: '#2dd4bf', backgroundColor: '#2dd4bf' }}
+            style={{ color: '#2dd4bf', filter: 'invert(1)' }}
         />
       </div>
       <textarea
@@ -60,14 +64,19 @@ export default function Home() {
       />
       <button
         onClick={handleGenerateRecipes}
-        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+        className="bg-teal-500 hover:bg-teal-600 text-white px-4 py-2 rounded"
       >
         Generate Recipes
       </button>
       {recipes && (
         <div className="mt-4">
-          <h2 className="text-2xl font-bold mb-2">Recipes:</h2>
-          <pre className="whitespace-pre-wrap">{recipes}</pre>
+          <h2 className="text-2xl font-bold mb-2 text-teal-400">Recipes:</h2>
+          <pre className="whitespace-pre-wrap text-teal-50">{recipes}</pre>
+        </div>
+      )}
+      {loading && (
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold mb-2 text-teal-400">Loading...</h2>
         </div>
       )}
     </div>
